525e7869b8e6ba4db702cfb55424392d5fb5019b
Drishti Kirodiwal <84236817+Drishti2003@users.noreply.github.com>
2023-08-28 20:53:35 +0530

    Update script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,47 +1,50 @@
-/*  let js = "amazing";
-// if (js === "amazing") alert("JavaScript is FUN!");
+// ******BASICS******
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is FUN!");
 console.log(20 + 4 - 10);
 console.log("Drishti");
-console.log(23);  */
+console.log(23);
 
-/*  let age = 18;
-// age = 19;     possible
+let $age = 18;
+// $age = 19;     possible
 
-const birthYear = 2003;
-// birthYear = 2002;     Not possible
-// const birthMonth;     Not possible
+const $birthYear = 2003;
+// $birthYear = 2002;     Not possible
+// const $birthMonth;     Not possible
 
 var _age = 18;
-// _age = 19;     possible  */
+// _age = 19;     possible
 
-/*  const now = 2023;
+// ******OPERATORS******
+const now = 2023;
 const ageDrishti = now - 2003;
 const ageXYZ = now - 2002;
 console.log(ageDrishti, ageXYZ);
-console.log(ageDrishti * 2, ageDrishti / 2, 2 ** 3); // multiply,division,power  
+console.log(ageDrishti * 2, ageDrishti / 2, 2 ** 3); // multiply,division,power
 
-const firstName = "Drishti";
+const $firstName = "Drishti";
 const lastName = "kirodiwal";
-console.log(firstName + " " + lastName); //concatenation  
+console.log($firstName + " " + lastName); //concatenation
 
 let x = 10 + 5;
 console.log(x);
 x += 10; // x = x + 10      +=, -=, /=, *=
 console.log(x);
 x = x++; // ++x, x--, --x
-console.log(x);  
+console.log(x);
 
-console.log(ageDrishti > ageXYZ); // >, <, >=, <=  */
+console.log(ageDrishti > ageXYZ); // >, <, >=, <=
 
-// let massMark = 78,
-//   heightMark = 1.69,
-//   massJohn = 92,
-//   heightJohn = 1.95;
+// ******CODING CHALLENGE ******
+let massMark = 78,
+  heightMark = 1.69,
+  massJohn = 92,
+  heightJohn = 1.95;
 
-/*  let massMark = 95,
-  heightMark = 1.88,
-  massJohn = 85,
-  heightJohn = 1.76;
+// let massMark = 95,
+//   heightMark = 1.88,
+//   massJohn = 85,
+//   heightJohn = 1.76;
 
 BMI_Mark = massMark / heightMark ** 2;
 BMI_John = massJohn / (heightJohn * heightJohn);
@@ -54,25 +57,27 @@ if (BMI_John > BMI_Mark) {
 
 markHigherBMI = BMI_Mark > BMI_John;
 
-console.log(BMI_John, BMI_Mark, markHigherBMI);  */
+console.log(BMI_John, BMI_Mark, markHigherBMI);
 
-/*  const firstName = "Drishti",
+// ******STRING & TEMPLATE******
+const firstName = "Drishti",
   job = "Student",
-  birthYear = 2003,
+  _birthYear = 2003,
   year = 2023;
 
 const drishti =
-  "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+  "I'm " + firstName + ", a " + (year - _birthYear) + " years old " + job + "!";
 console.log(drishti);
 
-const drishtiNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+const drishtiNew = `I'm ${firstName}, a ${year - _birthYear} years old ${job}!`;
 console.log(drishtiNew);
 
 console.log("Drishti\nKirodiwal");
 console.log(`Drishti
-Kirodiwal`);  */
+Kirodiwal`);
 
-/*  const age = 15;
+// ****** IF ELSE ******
+const age = 15;
 if (age >= 18) {
   console.log("He/She can start driving 🚗");
 } else {
@@ -81,10 +86,13 @@ if (age >= 18) {
 }
 
 let century;
-const birthYear = 2003;
-if (birthYear <= 2000) {
+const BirthYear = 2003;
+if (BirthYear <= 2000) {
   century = 20;
 } else {
   century = 21;
 }
-console.log(century);  */
+console.log(century);
+
+// CONVERSION & COERCION
+const birthYear = "";
