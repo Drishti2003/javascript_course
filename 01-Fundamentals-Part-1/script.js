eb3e75cbadb0daabd752622b94d23fcce72cb069
Drishti Kirodiwal <84236817+Drishti2003@users.noreply.github.com>
2023-08-29 23:20:23 +0530

    Coding challenge #3 done

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,5 @@
-// ******BASICS******
-let js = "amazing";
+// ******BASICS****** //
+/*  let js = "amazing";
 if (js === "amazing") alert("JavaScript is FUN!");
 console.log(20 + 4 - 10);
 console.log("Drishti");
@@ -13,10 +13,10 @@ const $birthYear = 2003;
 // const $birthMonth;     Not possible
 
 var _age = 18;
-// _age = 19;     possible
+// _age = 19;     possible  */
 
-// ******OPERATORS******
-const now = 2023;
+// ******OPERATORS****** //
+/*  const now = 2023;
 const ageDrishti = now - 2003;
 const ageXYZ = now - 2002;
 console.log(ageDrishti, ageXYZ);
@@ -33,10 +33,10 @@ console.log(x);
 x = x++; // ++x, x--, --x
 console.log(x);
 
-console.log(ageDrishti > ageXYZ); // >, <, >=, <=
+console.log(ageDrishti > ageXYZ); // >, <, >=, <=  */
 
-// ******CODING CHALLENGE ******
-let massMark = 78,
+// ******CODING CHALLENGE ****** //
+/*  let massMark = 78,
   heightMark = 1.69,
   massJohn = 92,
   heightJohn = 1.95;
@@ -57,10 +57,30 @@ if (BMI_John > BMI_Mark) {
 
 markHigherBMI = BMI_Mark > BMI_John;
 
-console.log(BMI_John, BMI_Mark, markHigherBMI);
+console.log(BMI_John, BMI_Mark, markHigherBMI);  
 
-// ******STRING & TEMPLATE******
-const firstName = "Drishti",
+const avgKoala = (88 + 91 + 110) / 3;
+const avgDolphin = (96 + 108 + 89) / 3;
+
+if (avgDolphin === avgKoala) console.log("It's a Draw!");
+else if (avgKoala > avgDolphin) console.log("Koala is winner!");
+else console.log("Dolphin is winner!");  
+
+// Bonus 1-2
+const avgKoala = (109 + 95 + 10) / 3;
+const avgDolphin = (97 + 112 + 10) / 3;
+console.log(avgDolphin, avgKoala);
+
+if (avgDolphin === avgKoala && avgDolphin >= 100 && avgKoala >= 100)
+  console.log("It's a Draw!");
+else if (avgKoala > avgDolphin && avgKoala >= 100)
+  console.log("Koala is winner!");
+else if (avgDolphin > avgKoala && avgDolphin >= 100)
+  console.log("Dolphin is winner!");
+else console.log("No one wins");  */
+
+// ******STRING & TEMPLATE****** //
+/*  const firstName = "Drishti",
   job = "Student",
   _birthYear = 2003,
   year = 2023;
@@ -74,10 +94,10 @@ console.log(drishtiNew);
 
 console.log("Drishti\nKirodiwal");
 console.log(`Drishti
-Kirodiwal`);
+Kirodiwal`);  */
 
-// ****** IF ELSE ******
-const age = 15;
+// ****** IF ELSE ****** //
+/*  const age = 15;
 if (age >= 18) {
   console.log("He/She can start driving 🚗");
 } else {
@@ -92,7 +112,66 @@ if (BirthYear <= 2000) {
 } else {
   century = 21;
 }
-console.log(century);
+console.log(century);  */
 
-// CONVERSION & COERCION
-const birthYear = "";
+// ******CONVERSION & COERCION****** //
+// type conversion
+/*  const inputYear = "1991";
+console.log(inputYear + 18);
+console.log(Number(inputYear));
+console.log(Number(inputYear) + 18);
+
+console.log(Number("jonas"));
+console.log(typeof NaN);
+
+console.log(String(23));  
+
+// type coercion
+console.log("I am " + 20 + " years old.");
+console.log("23" + "10" + 3);
+console.log("23" - "10" - 3); //same for * and /  */
+
+// ******TRUTHY & FALSY****** //
+// 5 falsy values : 0, '', undefined, null, NaN
+/*  console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean("Drishti"));
+console.log(Boolean({}));
+
+const money = 0;
+if (money) {
+  console.log("Don't spend!");
+} else {
+  console.log("You should get a job!");
+}  */
+
+// ******EQUALITY OPERATOR****** //
+/*  // const age = 18;
+const age = "18";
+if (age === 18) console.log("You just became an adult!(Strict)");
+if (age == 18) console.log("You just became an adult!(Loose)");
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+if (favourite === 27) console.log("Number is 27!");
+// not equal : !=(loose)  !==(strict)  */
+
+// ****** LOGICAL OPERATOR****** //
+/*  const hasDriversLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+if (shouldDrive) console.log("Able to drive!");
+else console.log("Not able to drive");
+
+const ifTired = false;
+
+if (hasDriversLicense && hasGoodVision && !ifTired)
+  console.log("Able to drive!");
+else console.log("Not able to drive");  */
