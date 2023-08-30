00b318fa94a47f1de9a865b718d2c09414d7f24a
Drishti Kirodiwal <84236817+Drishti2003@users.noreply.github.com>
2023-08-30 23:26:39 +0530

    Finished

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -79,6 +79,13 @@ else if (avgDolphin > avgKoala && avgDolphin >= 100)
   console.log("Dolphin is winner!");
 else console.log("No one wins");  */
 
+const bill = 430;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+
+console.log(
+  `Bill was ${bill} and tip was ${tip} and the total is ${bill + tip}`
+);
+
 // ******STRING & TEMPLATE****** //
 /*  const firstName = "Drishti",
   job = "Student",
@@ -175,3 +182,47 @@ const ifTired = false;
 if (hasDriversLicense && hasGoodVision && !ifTired)
   console.log("Able to drive!");
 else console.log("Not able to drive");  */
+
+// ****** SWITCH STATEMENT ****** //
+/*  const day = "monday";
+
+switch (day) {
+  case "monday": //day === 'monday'
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code example");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend!");
+    break;
+  default:
+    console.log("Not a valid day!");  
+} 
+
+if (day === "monday") {
+  console.log("Plan course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") console.log("Prepare theory videos");
+else if (day === "wednesday" || day === "thursday")
+  console.log("Write code example");
+else if (day === "friday") console.log("Record videos");
+else if (day === "saturday" || day === "sunday")
+  console.log("Enjoy the weekend!");
+else console.log("Not a valid day!");  */
+
+// ****** CONDITIONAL(TERNARY OPERATOR ****** //
+/* const age = 20;
+age >= 18 ? console.log("You can drive!") : console.log("You can't drive!");
+
+const drive = age >= 18 ? "can" : "can't";
+console.log(drive);  */
