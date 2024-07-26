c93417570683933725f117cb15eed5fb0c7c4ef0
Drishti Kirodiwal <84236817+Drishti2003@users.noreply.github.com>
2024-07-26 22:52:55 +0530

    Update script.js

--- 05-Working-with-Arrays/script.js
+++ 05-Working-with-Arrays/script.js
@@ -164,3 +164,29 @@ console.log(depositsFor);
 const withdrawal = movements.filter(mov => mov < 0);
 console.log(withdrawal);
 */
+
+///////////////////////////////////////////////// THE REDUCE METHOD /////////////////////////////////////////////////
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// accumulator -> snowball
+// const balance = movements.reduce(function (acc, mov, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + mov;
+// }, 0); //here acc=0
+
+const balance = movements.reduce((acc, mov) => acc + mov, 0);
+
+console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
+*/
