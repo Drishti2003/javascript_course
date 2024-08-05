317873b19ce3ec6e83ba4d8fd53895b6d4c470c2
Drishti Kirodiwal <s.sainibabli@gmail.com>
2024-08-05 19:26:40 +0530

    update script.js

--- 08 - Object-Oriented Programming (OOP) With JavaScript/script.js
+++ 08 - Object-Oriented Programming (OOP) With JavaScript/script.js
@@ -1,1 +1,27 @@
-'use strict';
+"use strict";
+
+///////////////////////////////////////////////// Constructor Functions and the New Operator /////////////////////////////////////////////////
+
+const Person = function (firstName, birthYear) {
+  // instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //   Never do this
+//   this.calcAge = function () {
+//     console.log(2037 - this.birthYear);
+//   };
+};
+
+const drishti = new Person("drishti", 2003);
+console.log(drishti);
+
+// 1. New empty object {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4.function automatically return {}
+
+const tanu = new Person("Tanu", 2002);
+console.log(tanu);
+
+console.log(drishti instanceof Person);
