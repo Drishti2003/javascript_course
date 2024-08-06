a315acaf2d71ee59028d5e2a76ede52f7a6e2969
Drishti Kirodiwal <s.sainibabli@gmail.com>
2024-08-06 17:17:33 +0530

    update script.js

--- 08 - Object-Oriented Programming (OOP) With JavaScript/script.js
+++ 08 - Object-Oriented Programming (OOP) With JavaScript/script.js
@@ -1,13 +1,13 @@
 "use strict";
 
 ///////////////////////////////////////////////// Constructor Functions and the New Operator /////////////////////////////////////////////////
-
+/*
 const Person = function (firstName, birthYear) {
   // instance properties
   this.firstName = firstName;
   this.birthYear = birthYear;
 
-  //   Never do this
+//   Never do this
 //   this.calcAge = function () {
 //     console.log(2037 - this.birthYear);
 //   };
@@ -25,3 +25,34 @@ const tanu = new Person("Tanu", 2002);
 console.log(tanu);
 
 console.log(drishti instanceof Person);
+*/
+
+///////////////////////////////////////////////// Prototypes /////////////////////////////////////////////////
+
+const Person = function (firstName, birthYear) {
+  // instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const drishti = new Person("drishti", 2003);
+
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2024 - this.birthYear);
+};
+
+drishti.calcAge();
+
+console.log(drishti.__proto__);
+console.log(drishti.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(drishti));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = "Homo Sapiens";
+console.log(drishti);
+
+console.log(drishti.hasOwnProperty("firstName"));
+console.log(drishti.hasOwnProperty("species"));
