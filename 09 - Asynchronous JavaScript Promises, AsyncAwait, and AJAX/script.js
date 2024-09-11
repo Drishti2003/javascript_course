d9057e5ad825d54eaa43394fd83d61d575253746
Drishti Kirodiwal <s.sainibabli@gmail.com>
2024-09-11 21:01:13 +0530

    update script.js

--- 09 - Asynchronous JavaScript Promises, AsyncAwait, and AJAX/script.js
+++ 09 - Asynchronous JavaScript Promises, AsyncAwait, and AJAX/script.js
@@ -7,6 +7,8 @@ const countriesContainer = document.querySelector(".countries");
 
 //   https://countries-api-836d.onrender.com/countries/
 
+///////////////////////////////////////  Our First AJAX Call XMLHttpRequest  ///////////////////////////////////////
+/*
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open(
@@ -40,3 +42,77 @@ const getCountryData = function (country) {
 
 getCountryData('republic of india')
 getCountryData('usa')
+*/
+
+///////////////////////////////////////  Welcome to Callback Hell  ///////////////////////////////////////
+
+/*
+const renderCountry = function (data, className = "") {
+  const html = `
+  <article class="country ${className}">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+  </article>
+`;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open(
+    "GET",
+    `https://restcountries.com/v2/name/${country}?fullText=true`
+  );
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neightbour country (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener("load", function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, "neighbour");
+    });
+  });
+};
+
+getCountryAndNeighbour("republic of india");
+*/
+
+///////////////////////////////////////  Promises and the Fetch API  ///////////////////////////////////////
+
+// const request = new XMLHttpRequest();
+// request.open(
+//   "GET",
+//   `https://restcountries.com/v2/name/${country}?fullText=true`
+// );
+// request.send();
+
+const request = fetch(
+  "https://restcountries.com/v2/name/republic of india?fullText=true"
+);
