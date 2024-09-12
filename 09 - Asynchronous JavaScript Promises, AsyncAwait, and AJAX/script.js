2c846a5c2c07f74b48e941c7a9d5d4f74f994ab6
Drishti Kirodiwal <s.sainibabli@gmail.com>
2024-09-12 23:21:57 +0530

    script.js updated

--- 09 - Asynchronous JavaScript Promises, AsyncAwait, and AJAX/script.js
+++ 09 - Asynchronous JavaScript Promises, AsyncAwait, and AJAX/script.js
@@ -240,6 +240,7 @@ btn.addEventListener("click", function () {
 
 ///////////////////////////////////////  Coding Challenge #1  ///////////////////////////////////////
 
+/*
 const whereAmI = function (lat, lng) {
   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
     .then((res) => {
@@ -266,3 +267,20 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+*/
+
+///////////////////////////////////////  The Event Loop  ///////////////////////////////////////
+
+/*
+console.log("Test start");
+setTimeout(() => console.log("0 sec timer"), 0); //calback queue
+Promise.resolve("Resolved promise 1").then((res) => console.log(res)); //microtask queue
+
+Promise.resolve("Resolved promise 2").then((res) => {
+  for (let i = 0; i < 100000; i++) {}
+  console.log(res);
+});
+console.log("test end");
+
+// microtask queue has high priority than callback queue
+*/
