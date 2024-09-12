df1b50ded1e00221dd4fd550ba0312b571c8e208
Drishti Kirodiwal <s.sainibabli@gmail.com>
2024-09-12 21:27:15 +0530

    updated script.js

--- 09 - Asynchronous JavaScript Promises, AsyncAwait, and AJAX/script.js
+++ 09 - Asynchronous JavaScript Promises, AsyncAwait, and AJAX/script.js
@@ -3,6 +3,30 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
+const renderCountry = function (data, className = "") {
+  const html = `
+  <article class="country ${className}">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+  </article>
+`;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText("beforeend", msg);
+  countriesContainer.style.opacity = 1;
+};
+
 ///////////////////////////////////////
 
 //   https://countries-api-836d.onrender.com/countries/
@@ -106,25 +130,6 @@ getCountryAndNeighbour("republic of india");
 
 ///////////////////////////////////////  Promises and the Fetch API  ///////////////////////////////////////
 
-const renderCountry = function (data, className = "") {
-  const html = `
-  <article class="country ${className}">
-  <img class="country__img" src="${data.flag}" />
-  <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-  </div>
-  </article>
-`;
-  countriesContainer.insertAdjacentHTML("beforeend", html);
-  countriesContainer.style.opacity = 1;
-};
-
 // const request = fetch(
 //   "https://restcountries.com/v2/name/republic of india?fullText=true"
 // );
@@ -169,7 +174,20 @@ const getCountryData = function (country) {
       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
     })
     .then((response) => response.json())
-    .then((data) => renderCountry(data, "neighbour"));
+    .then((data) => renderCountry(data, "neighbour"))
+
+    //////////// Handling rejected promises
+    .catch((err) => {
+      console.error(`${err} 💥💥💥`);
+      renderError(`Something went wrong ${err.message}`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
 };
 
-getCountryData("republic of india");
+btn.addEventListener("click", function () {
+  getCountryData("republic of india");
+});
+
+getCountryData("fgdfskbdl");
