aa046c57895ecc7f4ca36940c8910ab9cdc9e49a
Drishti Kirodiwal <s.sainibabli@gmail.com>
2024-09-13 18:44:33 +0530

    script.js updated

--- 09 - Asynchronous JavaScript Promises, AsyncAwait, and AJAX/script.js
+++ 09 - Asynchronous JavaScript Promises, AsyncAwait, and AJAX/script.js
@@ -284,3 +284,52 @@ console.log("test end");
 
 // microtask queue has high priority than callback queue
 */
+
+///////////////////////////////////////  Building a simple promise  ///////////////////////////////////////
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log("Lottery draw is happening 🔮");
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve("You WIN 💰");
+    } else {
+      reject(new Error("You lost your money 💩"));
+    }
+  }, 2000);
+});
+
+lotteryPromise
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+// wait(2)
+//   .then(() => {
+//     console.log("I waited for 2 seconds");
+//     return wait(1);
+//   })
+//   .then(() => console.log("I waited for 1 seconds"));
+
+wait(1)
+  .then(() => {
+    console.log("I waited for 1 seconds");
+    return wait(1);
+  })
+  .then(() => {
+    console.log("I waited for 2 seconds");
+    return wait(1);
+  })
+  .then(() => {
+    console.log("I waited for 3 seconds");
+    return wait(1);
+  })
+  .then(() => console.log("I waited for 4 seconds"));
+
+Promise.resolve("abc").then((x) => console.log(x));
+Promise.reject("abc").catch((x) => console.error(x));
