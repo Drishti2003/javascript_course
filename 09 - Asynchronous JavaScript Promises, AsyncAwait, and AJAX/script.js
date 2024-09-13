efcff76ee2e93f7b59cc46b3e533fc0326e79f42
Drishti Kirodiwal <s.sainibabli@gmail.com>
2024-09-13 21:22:22 +0530

    script.js updated

--- 09 - Asynchronous JavaScript Promises, AsyncAwait, and AJAX/script.js
+++ 09 - Asynchronous JavaScript Promises, AsyncAwait, and AJAX/script.js
@@ -287,6 +287,7 @@ console.log("test end");
 
 ///////////////////////////////////////  Building a simple promise  ///////////////////////////////////////
 
+/*
 const lotteryPromise = new Promise(function (resolve, reject) {
   console.log("Lottery draw is happening 🔮");
   setTimeout(function () {
@@ -333,3 +334,48 @@ wait(1)
 
 Promise.resolve("abc").then((x) => console.log(x));
 Promise.reject("abc").catch((x) => console.error(x));
+*/
+
+///////////////////////////////////////  Promisifying the Geolocation API  ///////////////////////////////////////
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    //   navigator.geolocation.getCurrentPosition(
+    //     (position) => resolve(position),
+    //     (err) => reject(err)
+    //   );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+getPosition().then((pos) => console.log(pos));
+
+const whereAmI = function () {
+  getPosition()
+    .then((pos) => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+
+      return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    })
+    .then((res) => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+
+      return fetch(
+        `https://restcountries.com/v2/name/${data.country}?fullText=true`
+      );
+    })
+    .then((res) => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+      return res.json();
+    })
+    .then((data) => renderCountry(data[0]))
+    .catch((err) => console.error(`${err.message} 💥`));
+};
+
+btn.addEventListener("click", whereAmI);
