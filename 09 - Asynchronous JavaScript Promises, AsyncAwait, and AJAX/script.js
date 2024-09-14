0b397a025361e8ac3dd907b9de4ce5c22ab3af1f
Drishti Kirodiwal <s.sainibabli@gmail.com>
2024-09-14 16:49:46 +0530

    Finished

--- 09 - Asynchronous JavaScript Promises, AsyncAwait, and AJAX/script.js
+++ 09 - Asynchronous JavaScript Promises, AsyncAwait, and AJAX/script.js
@@ -605,6 +605,7 @@ get3Countries("portugal", "canada", "republic of india");
 
 ///////////////////////////////////////  Other Promise Combinators: race, allSettled and any  ///////////////////////////////////////
 
+/*
 // Promise.race
 (async function () {
   const res = await Promise.race([
@@ -654,3 +655,87 @@ Promise.any([
 ])
   .then((res) => console.log(res))
   .catch((err) => console.error(err));
+*/
+
+///////////////////////////////////////  Coding Challenge #3  ///////////////////////////////////////
+
+/*
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector(".images");
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = imgPath;
+
+    img.addEventListener("load", function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener("error", function () {
+      reject(new Error("Image not found"));
+    });
+  });
+};
+
+let currentImg;
+
+// createImage("img/img-1.jpg")
+//   .then((img) => {
+//     currentImg = img;
+//     console.log("Image 1 loaded");
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = "none";
+//     return createImage("img/img-2.jpg");
+//   })
+//   .then((img) => {
+//     currentImg = img;
+//     console.log("Image 2 loaded");
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = "none";
+//   })
+//   .catch((err) => console.error(err));
+
+// const loadNPause = async function () {
+//   try {
+//     // Load image 1
+//     let img = await createImage("img/img-1.jpg");
+//     console.log("Image 1 loaded");
+//     await wait(2);
+//     img.style.display = "none";
+
+//     // Load image 2
+//     img = await createImage("img/img-2.jpg");
+//     console.log("Image 2 loaded");
+//     await wait(2);
+//     img.style.display = "none";
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+// loadNPause();
+
+const loadAll = async function (imagArr) {
+  try {
+    const imgs = imagArr.map(async (img) => await createImage(img));
+    console.log(imgs);
+
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+    imgsEl.forEach((img) => img.classList.add("parallel"));
+  } catch (err) {
+    console.error(err);
+  }
+};
+loadAll(["img/img-1.jpg", "img/img-2.jpg", "img/img-3.jpg"]);
+*/
