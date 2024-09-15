5bb5271e75e93067da1c7447f43ab48bbd853a01
Drishti Kirodiwal <s.sainibabli@gmail.com>
2024-09-15 19:03:59 +0530

    Script.js updated

--- 10 - Modern JavaScript Development Modules, Tooling, and Functional/script.js
+++ 10 - Modern JavaScript Development Modules, Tooling, and Functional/script.js
@@ -1,3 +1,5 @@
+///////////////////////////////////////  Exporting and Importing in ES6 Modules  ///////////////////////////////////////
+
 // Importing module
 // import { addToCart, totalPrice as price, qt } from "./shoppingCart.js";
 // addToCart("bread", 5);
@@ -19,3 +21,27 @@ add("bread", 5);
 add("apple", 4);
 
 console.log(cart);
+
+///////////////////////////////////////  Top-Level Await (ES2022)  ///////////////////////////////////////
+
+// console.log("Start fetching");
+// const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+// const data = await res.json();
+// console.log(data);
+// console.log("Something");
+
+const getLastPost = async function () {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+// lastPost.then((last) => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
