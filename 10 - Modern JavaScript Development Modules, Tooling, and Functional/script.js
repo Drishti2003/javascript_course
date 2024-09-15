c7c0356456273d057c1c056d0a7cd4247fce819e
Drishti Kirodiwal <s.sainibabli@gmail.com>
2024-09-15 20:16:23 +0530

    script.js updated

--- 10 - Modern JavaScript Development Modules, Tooling, and Functional/script.js
+++ 10 - Modern JavaScript Development Modules, Tooling, and Functional/script.js
@@ -5,7 +5,7 @@
 // addToCart("bread", 5);
 // console.log(price, qt);
 
-console.log("Importing module");
+// console.log("Importing module");
 // console.log(shippingCost);
 
 // import * as ShoppingCart from "./shoppingCart.js";
@@ -15,15 +15,16 @@ console.log("Importing module");
 // import add, { addToCart, totalPrice as price, qt } from "./shoppingCart.js";
 // console.log(price);
 
-import add, { cart } from "./shoppingCart.js";
-add("pizza", 2);
-add("bread", 5);
-add("apple", 4);
+// import add, { cart } from "./shoppingCart.js";
+// add("pizza", 2);
+// add("bread", 5);
+// add("apple", 4);
 
-console.log(cart);
+// console.log(cart);
 
 ///////////////////////////////////////  Top-Level Await (ES2022)  ///////////////////////////////////////
 
+/*
 // console.log("Start fetching");
 // const res = await fetch("https://jsonplaceholder.typicode.com/posts");
 // const data = await res.json();
@@ -45,3 +46,47 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+
+///////////////////////////////////////  The Module Pattern  ///////////////////////////////////////
+
+/*
+const shoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+shoppingCart2.addToCart("apple", 4);
+shoppingCart2.addToCart("pizza", 2);
+console.log(shoppingCart2);
+console.log(shoppingCart2.shippingCost);
+*/
+
+///////////////////////////////////////  CommonJS Modules  ///////////////////////////////////////
+
+// export
+//  export.addToCart = function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to cart`);
+// };
+
+// // Import
+// const {addToCart} = require('./shoppingCart.js');
